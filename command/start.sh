0d11108b2e6fe085ee2b9cf7a4de34d3210bd49a
bb <b.bakov@gmail.com>
2019-11-20 01:22:01 +0200

    Start to port to require from import modules

---
+++ command/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "start server and watch command.";
+
+npm run build:watch
---
+++ test_lib/store.test.js
@@ -0,0 +1,5 @@
+var expect = require("chai").expect;
+var sinon = require("sinon");
+var Store = require("../build2/store.min");
+
+console.log("STORE : ", bundle);
