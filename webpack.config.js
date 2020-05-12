f38c1d6df2b363728cfed0fea45e610928f0b847
b bakov <b.bakov@gmail.com>
2020-05-13 00:41:24 +0300

    Improve: pack snapSvg into lines bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,24 @@
 module.exports = {
-  entry: './lib/index.js',
+  entry: [
+    "snapsvg/dist/snap.svg.js",
+    "./lib/index.js"
+  ],
   output: {
-    libraryTarget: 'umd',
-    library: 'Lines',
+    libraryTarget: "umd",
+    library: "Lines",
   },
   // target: 'node',
   module: {
     rules: [
       {
+        test: require.resolve('snapsvg/dist/snap.svg.js'),
+        use: 'imports-loader?this=>window,fix=>module.exports=0',
+      },
+      {
         test: /\.js?$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
-  }
+        use: ["babel-loader"],
+      },
+    ],
+  },
 };
